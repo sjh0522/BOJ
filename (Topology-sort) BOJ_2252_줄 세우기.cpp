f3a67006b1ca9e2833a538c3829827ab17bed8ee
sjh0522 <74394708+sjh0522@users.noreply.github.com>
2020-11-21 16:55:06 +0900

    Update (Topology-sort) BOJ_2252_줄 세우기.cpp

--- (Topology-sort) BOJ_2252_줄 세우기.cpp
+++ (Topology-sort) BOJ_2252_줄 세우기.cpp
@@ -30,25 +30,26 @@ int main(void) {
 	   2-3. adj 노드 탐색하여 진입 차수 0인 노드 queue에 삽입 */
 	
 	
-	// 1. 진입 차수 0 인 노드 queue에 삽입
+	// 1. 초기화 : 진입 차수 0 인 노드 queue에 삽입
 	vector<int> result;
 	queue<int> q;
 	for (int i = 1; i <= N; i++) {
 		if (inDegree[i] == 0) q.push(i);
 	}
 
-	// 2. N노드 모두 탐색
+	// 2. N 노드 탐색
 	for (int i = 1; i <= N; i++) {
-		// 2-1. queue가 비면 cycle발생하여 종료
+		// 2-1. 종료조건 : queue가 비면 cycle발생
 		if (q.empty()) break;
 
-		// 2-2. queue에서 꺼내 간선 제거 후 result에 넣음
+		// 2-2. currnode queue -> result
 		int currnode = q.front(); q.pop();
 		result.push_back(currnode);
 
-		// 2-3. adj 노드 탐색
+		// 2-3. adj 탐색
 		for (int j = 0; j < adj[currnode].size(); j++) {
 			int nextnode = adj[currnode][j];
+			// 2-4. nextnode inDegree 간선 제거 후 queue에 삽입
 			if (--inDegree[nextnode] == 0) q.push(nextnode);
 		}
 	}
